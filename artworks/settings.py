640f7155a419a1cc7a759ff6f7b18c3e757175d1
IsacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2023-11-11 21:40:10 -0800

    fix: staticfiles

--- artworks/settings.py
+++ artworks/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "collection"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'collection/static'),
+)
 LOGOUT_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
